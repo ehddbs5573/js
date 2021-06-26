d806fc70fa1dac65452f688260b3c29b0451fdcd
pink bean <ehddbs5573@naver.com>
2021-06-26 15:22:16 +0900

    updated username

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const loginForm=document.querySelector("#login-form");
 const loginInput=loginForm.querySelector("input");
+const loginBtn=loginForm.querySelector("button");
+const greeting=document.querySelector("#greeting");
 
-function onLoginSubmit(){
+
+function onLoginSubmit(event){
+    const HIDDEN_CLASSNAME = "hidden"
+
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
+    
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText=`welcome to ${username}`;
+
+    localStorage.setItem("username",username);
+    
 }
+
 
 loginForm.addEventListener("submit",onLoginSubmit);
