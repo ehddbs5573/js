dadb236cc686c88e084137b78ba158e86a869f3a
pink bean <ehddbs5573@naver.com>
2021-06-28 14:45:45 +0900

    updated clock quotes

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hour = String(date.getHours()).padStart(2,"0");
+    const minute = String(date.getMinutes()).padStart(2,"0"); 
+    const second = String(date.getSeconds()).padStart(2,"0");
+    
+    clock.innerText=`${hour}:${minute}:${second}`;
 }
 getClock()
 setInterval(getClock, 1000);
